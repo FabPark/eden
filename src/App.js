dde36ce7323b471fed15f8caa2eb169eaa09f479
FabPark <fabpark93@gmail.com>
2023-02-04 16:04:24 +0900

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+const Hi = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div> Hi there. </div>
+  )
 }
+function App() {
+ const [number, setNumber] = useState(0);
+
+ const add = () => {
+  setNumber(number + 1)
+ }
+
+ return (
+  <div className="App">
+    <Hi />
+    <div> Number={number}</div>
+    <button onClick = {add}>Add</button>
+    </div>
+
+  );
+ }
 
 export default App;
