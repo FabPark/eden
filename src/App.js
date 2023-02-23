7ca54ee4be3b1d0d007150d08b26d1f3c09839cc
christopher@goodpairdays.com <christopher@goodpairdays.com>
2023-02-24 08:33:24 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi Fabian, this is a good start for us to start building the todo app!
+        </p>
+        <h4>
+          Learn React
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
