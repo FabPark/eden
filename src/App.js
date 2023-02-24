079bf2df583d1dc55472e774089f32f8797be8eb
christopher@goodpairdays.com <christopher@goodpairdays.com>
2023-02-24 09:02:30 +0900

    add code for todo functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEditTodo = (id, updatedTodo) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, ...updatedTodo };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Fabian, this is a good start for us to start building the todo app!
-        </p>
-        <h4>
-          Learn React
-        </h4>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <TodoForm onAddTodo={handleAddTodo} />
+      <TodoList
+        todos={todos}
+        onDeleteTodo={handleDeleteTodo}
+        onEditTodo={handleEditTodo}
+      />
     </div>
   );
 }
 
-export default App;
+function TodoForm({ onAddTodo }) {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = { id: Date.now(), title };
+    onAddTodo(newTodo);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+}
+
+function TodoList({ todos, onDeleteTodo, onEditTodo }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.title}
+          <button onClick={() => onDeleteTodo(todo.id)}>Delete</button>
+          <button onClick={() => onEditTodo(todo.id, { title: 'New Title' })}>
+            Edit
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoApp;
